482426126b1e6f9283504c2a35723b73a4b5eb1a
bachtiarrizkyal <bachtiarrizkyalamsyah17@gmail.com>
2023-05-25 13:30:55 +0700

    Update Navbar dan Footer

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,9 +2,13 @@
 
 @section('title', 'Home')
 @section('content')
-<div class="flex  flex-col justify-center items-center h-screen space-y-4">
-    <h1 class="text-7xl text-blue-500 font-montserrat">KuyFit 2023 Montserrat</h1>
-    <h1 class="text-7xl text-green-500 font-poppins">KuyFit 2023 Popppins</h1>
-    <h1 class="text-7xl text-yellow-500 font-inter font">KuyFit 2023 Inter</h1>
+<div class="flex flex-col items-center justify-center h-screen space-y-4">
+    <h1 class="text-blue-500 text-7xl font-montserrat">KuyFit 2023 Montserrat</h1>
+    <h1 class="text-green-500 text-7xl font-poppins">KuyFit 2023 Popppins</h1>
+    <h1 class="text-yellow-500 text-7xl font-inter font">KuyFit 2023 Inter</h1>
 </div>
+@endsection
+
+@section('layouts.footer')
+
 @endsection
