0bf69a40d60afa7fe8d144df10f2bf0d72035823
Nathanael P Ganata <79926038+nathanaelpganata@users.noreply.github.com>
2023-05-31 19:51:41 +0700

    feat: add lapangan crud (#27)

--- app/Http/Controllers/DashboardLapangan.php
+++ app/Http/Controllers/DashboardLapangan.php
@@ -2,12 +2,105 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Lapangan;
 use Illuminate\Http\Request;
 
 class DashboardLapangan extends Controller
 {
     public function index()
     {
-        return view('dashboard.lapangan');
+        $lapangan = Lapangan::all();
+        return view('dashboard.lapangan', compact('lapangan'));
+    }
+    public function create()
+    {
+        return view('dashboard.tambahLapangan');
+    }
+
+    public function store(Request $request)
+    {
+        $request->merge([
+            'wifi' => $request->filled('wifi') ? $request->wifi : '0',
+            'toilet' => $request->filled('toilet') ? $request->toilet : '0',
+            'canteen' => $request->filled('canteen') ? $request->canteen : '0',
+            'musalla' => $request->filled('musalla') ? $request->musalla : '0',
+        ]);
+
+        $request->validate([
+            'venueName' => 'required',
+            'oprTime' => 'required',
+            'address' => 'required',
+            'phoneNumber' => 'required',
+            'wifi' => 'required',
+            'toilet' => 'required',
+            'canteen' => 'required',
+            'musalla' => 'required',
+            'photo' => 'required',
+            'sportId' => 'required',
+        ]);
+
+        // !! Uncomment
+        // $request['ownerId'] = Auth::user()->id;
+        $request['ownerId'] = 1;
+
+        Lapangan::create($request->all());
+
+        return redirect()->route('dashboard.lapangan')
+            ->with('success', 'Lapangan created successfully.');
+    }
+
+    public function show(Lapangan $lapangan, $id)
+    {
+        $lapangan = Lapangan::find($id);
+
+        return view('dashboard.detailLapangan', compact('lapangan'));
+    }
+
+    public function edit(Lapangan $lapangan, $id)
+    {
+        $lapangan = Lapangan::find($id);
+        return view('dashboard.editDetailLapangan', compact('lapangan'));
+    }
+
+    public function update(Request $request, Lapangan $lapangan, $id)
+    {
+        $request->merge([
+            'wifi' => $request->filled('wifi') ? $request->wifi : '0',
+            'toilet' => $request->filled('toilet') ? $request->toilet : '0',
+            'canteen' => $request->filled('canteen') ? $request->canteen : '0',
+            'musalla' => $request->filled('musalla') ? $request->musalla : '0',
+        ]);
+
+        $request->validate([
+            'venueName' => 'required',
+            'oprTime' => 'required',
+            'address' => 'required',
+            'phoneNumber' => 'required',
+            'wifi' => 'required',
+            'toilet' => 'required',
+            'canteen' => 'required',
+            'musalla' => 'required',
+            'photo' => 'required',
+            'sportId' => 'required',
+        ]);
+        // !! Uncomment
+        // $request['ownerId'] = Auth::user()->id;
+        $request['ownerId'] = 1;
+
+        $lapangan = Lapangan::find($id);
+        $lapangan->update($request->all());
+
+        return redirect()->route('dashboard.lapangan')
+            ->with('success', 'Lapangan updated successfully.');
+    }
+
+    public function destroy(Lapangan $lapangan, $id)
+    {
+        $lapangan = Lapangan::find($id);
+        $lapangan->delete();
+        return redirect()->route('dashboard.lapangan')
+            ->with('success', 'Lapangan deleted successfully.');
     }
 }
