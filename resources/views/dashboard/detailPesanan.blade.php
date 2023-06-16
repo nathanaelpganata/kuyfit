18a1e97a3f35416ba549dcdf0376b38e97ff69b3
nathanaelpganata <nathanaelpg8@gmail.com>
2023-06-16 23:22:56 +0700

    fix: some adjustments

--- resources/views/dashboard/detailPesanan.blade.php
+++ resources/views/dashboard/detailPesanan.blade.php
@@ -9,23 +9,28 @@
             <div class="flex flex-row justify-between items-center m-10 mb-16">
                 <h1 class="text-4xl font-bold text-left">Pesanan-{{ $detail->id }}</h1>
                 @if ($detail->status == 'pending')
-                    <div class="space-x-2" x-data="{openReject: false}">
-                        <a href="/my/pesanan/detail/{{ $detail->id }}/accept"
-                            class="shadow bg-[#27698F] hover:brightness-110 focus:shadow-outline focus:outline-none text-white text-base font-bold tracking-wider py-2 px-4 rounded-lg">Konfirmasi</a>
-                            <div
-                            class="relative z-0" >
-                            <button @click="openReject = !openReject" class="bg-red-600 px-6 py-2 rounded-xl active:bg-red-800 hover:brightness-75">
-                                Reject
-                            </button>
-                            <form class="absolute z-50 flex flex-col justify-center items-center gap-3 w-72 h-auto sm:w-96 sm:h-auto bg-black rounded-lg top-16 -right-10 sm:right-4 p-4" x-show="openReject" action="/my/pesanan/detail/{{ $detail->id }}/reject?reason={{ $reason }}" method="POST">
-                                <div class="flex flex-row justify-between items-center w-full">
-                                    <label for="reason" class="text-xl font-bold text-white tracking-wider">Reason</label>
-                                    <p class="text-sm font-light text-white cursor-pointer" @click="openReject = false">Close</p>
-                                </div>
-                                <textarea type="text" placeholder="Place your reason for rejecting here.. (max: 255)." name="reason" id="reason" class="px-4 py-3 outline-none select-none text-white border-[1px] border-white rounded-lg w-full h-36 bg-transparent text-base" maxlength="255" ></textarea>
-                                <button type="submit" class="bg-red-600 py-1.5 text-base rounded-xl hover:brightness-75 w-full">Send</button>
-                            </form>
-                        </div>
+                    <div class="space-x-2 flex flex-row">
+                        <form action="/my/pesanan/detail/{{ $detail->id }}/action" method="POST">
+                            @csrf
+                            <input type="text" class="hidden" name="status" id="status" value="accept">
+                            <button type="submit"
+                                class="shadow bg-[#27698F] hover:brightness-110 focus:shadow-outline focus:outline-none text-white text-base font-bold tracking-wider py-2 px-4 rounded-lg">Konfirmasi</button>
+                        </form>
+                            <div class="relative z-50" x-data="{openReject: false}">
+                                <button @click="openReject = !openReject" class="bg-red-600 shadow hover:brightness-110 focus:shadow-outline focus:outline-none text-white text-base font-bold tracking-wider py-2 px-4 rounded-lg">
+                                    Reject
+                                </button>
+                                <form class="absolute z-50 flex flex-col justify-center items-center gap-3 w-72 h-auto sm:w-96 sm:h-auto bg-black rounded-lg top-16 -right-10 sm:right-4 p-4" x-show="openReject" action="/my/pesanan/detail/{{ $detail->id }}/action" method="POST">
+                                    @csrf
+                                    <div class="flex flex-row justify-between items-center w-full">
+                                        <label for="reason" class="text-xl font-bold text-white tracking-wider">Reason</label>
+                                        <p class="text-sm font-light text-white cursor-pointer" @click="openReject = false">Close</p>
+                                    </div>
+                                    <input type="text" class="hidden" name="status" id="status" value="reject">
+                                    <textarea type="text" placeholder="Place your reason for rejecting here.. (max: 255)." name="reason" id="reason" class="px-4 py-3 outline-none select-none text-white border-[1px] border-white rounded-lg w-full h-36 bg-transparent text-base" maxlength="255" ></textarea>
+                                    <button type="submit" class="bg-red-600 py-1.5 text-base rounded-xl hover:brightness-75 w-full">Send</button>
+                                </form>
+                            </div>
                         {{-- <a href="/my/pesanan/detail/{{ $detail->id }}/reject?reason=nothing"
                             class="shadow bg-[#445863] hover:brightness-110 focus:shadow-outline focus:outline-none text-white text-base font-bold tracking-wider py-2 px-4 rounded-lg">Tolak</a> --}}
                     </div>
--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <script src="//unpkg.com/alpinejs" defer></script>
     @vite('resources/css/app.css')
 </head>
 
