27ac53b38f3982f3a8d92b9025843e57baaec831
FahrulID <fahrulofficial@gmail.com>
2023-06-18 00:11:52 +0700

    fix: update detil lapangan

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -104,6 +104,11 @@ class VenueController extends Controller
         ]);
         $request['ownerId'] = Auth::user()->id;
 
+        $imageName = \Ramsey\Uuid\Uuid::uuid4()->toString() . time() . '.' . $request->photo->extension();
+        $request['photo']->storeAs('images/', $imageName);
+        $request['photo']->move(public_path('images/fotolapangan'), $imageName);
+        $request['photo'] = 'images/fotolapangan/' . $imageName;
+
         $lapangan = Lapangan::find($id);
         $lapangan->update($request->all());
 
